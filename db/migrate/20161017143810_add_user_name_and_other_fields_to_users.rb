03fa003ccbd3539c0053ceef0446d1d32fec5f52
laxmikanth1234 <laxmikanth.veggalam@gmail.com>
2016-11-04 12:39:08 +0530

    test

--- db/migrate/20161017143810_add_user_name_and_other_fields_to_users.rb
+++ db/migrate/20161017143810_add_user_name_and_other_fields_to_users.rb
@@ -4,7 +4,7 @@ class AddUserNameAndOtherFieldsToUsers < ActiveRecord::Migration[5.0]
     add_column :users, :date_of_birth, :date
     add_column :users, :gender, :boolean
     add_column :users, :batch, :int
-    remove_column :users, :gender,
-    add_column :users, :gender, :string,
+    remove_column :users, :gender
+    add_column :users, :gender, :string
   end
 end
