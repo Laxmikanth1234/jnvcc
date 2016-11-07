79606459c4bd2b0d10aa2eaaf1ccbc8a05453bb3
laxmi1 <laxmi@techvoxinc.com>
2016-11-07 13:27:23 +0530

    fixed few issues

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :user
    validates :name, :story, :when, :where, presence: true
-   has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png"
+   has_attached_file :avatar, :styles => { :medium => "600x600>", :thumb => "300x300#" }, :default_url => "/images/:style/missing.png"
    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 end
