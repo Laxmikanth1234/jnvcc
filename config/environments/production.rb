9b35dec37109f75e4a5455c5a9d8fb4863e473ce
laxmikanth1234 <laxmikanth.veggalam@gmail.com>
2016-11-30 10:37:47 +0530

    production email setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,18 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "jnvcc_#{Rails.env}"
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
 
+  config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "mail.gmail.com",
+  :user_name            => "laxmikanth@navodayans.org",
+  :password             => "next@123",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
+  config.action_mailer.default_url_options = { :host => 'www.navodayans.org' }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
