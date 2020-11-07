117e73b80ff70e629a87e6a07cf4594ddc68e7f4
laxmi1 <laxmi@techvoxinc.com>
2020-11-07 11:49:04 +0530

    fix CAPTCHA

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6LdbJeAZAAAAAE1U9LvPq2Ojf170EpriQHMVmmkH'
-  config.secret_key = '6LdbJeAZAAAAAH_dFPKcpsXSfIVOTHH4o438wcGd'
+  config.site_key  = '6LdgJuAZAAAAACe52zyTgk-en8rRWlOeNrP5HEAw'
+  config.secret_key = '6LdgJuAZAAAAAJcArwiNVadfv_tA2GlJ6Pxg7sna'
 end
